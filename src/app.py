32d04b00ccfdb213dd22bbb0a46a98be5b93a839
Daniel Espino Timón <akathorn@gmail.com>
2013-04-14 14:30:03 +0200

    Cielo y océano

--- src/app.py
+++ src/app.py
@@ -1,10 +1,17 @@
 import pygame
 from pygame.locals import *
 
+FPS_LIMIT = 60
+
 class App:
-    def __init__(self, size = (320, 240)):
+    def __init__(self, size = (800, 600)):
         self.size = size
+        width, height = size
         self.screen = pygame.display.set_mode(size)
+        self.clock = pygame.time.Clock()
+        self.ocean = pygame.Surface((width, height / 2))
+        self.ocean.fill((0,0,200))
+        self.ocean_rect = pygame.Rect(0,height/2,width,height/2)
 
     def start(self):
         while self.loop():
@@ -14,11 +21,17 @@ class App:
         screen = self.screen
         size = self.size
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
+        t = self.clock.tick(FPS_LIMIT)
 
-        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    return False
+
+        screen.fill((50,170,225))
+        screen.blit(self.ocean, self.ocean_rect)
 
         pygame.display.flip()
 
