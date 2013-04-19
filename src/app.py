c1439e1157e04229d7cbe0e030e31110a10d9cae
Daniel Espino Timón <akathorn@gmail.com>
2013-04-19 23:59:25 +0200

    Añadido módulo callbacks para la gestión de eventos

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@
 import pygame, sys
 from pygame.locals import *
 
+from utils.callbacks import *
 from game import TestGame
 from game import Menu
 
@@ -14,10 +15,8 @@ class App:
 
         self.clock = pygame.time.Clock()
 
-        self.callbacks = {KEYDOWN : {}, KEYUP : {}, QUIT : []}
-
-        self.register_event(QUIT, self.salir)
-        self.register_event(KEYDOWN, self.escape, key = K_ESCAPE)
+        register_event(QUIT, self.salir)
+        register_event(KEYDOWN, self.escape, key = K_ESCAPE)
 
         self.fps_t = 0
 
@@ -38,25 +37,6 @@ class App:
 
         pygame.display.set_icon(icon)
 
-
-    def register_event(self, evt, call, key = None):
-        if evt == KEYDOWN or evt == KEYUP:
-            if self.callbacks[evt].has_key(key):
-                self.callbacks[evt][key].append(call)
-            else:
-                self.callbacks[evt][key]=[call]
-        else:
-            self.callbacks[evt].append(call)
-
-    def unregister_event(self, evt, call, key = None):
-        if evt == KEYDOWN or evt == KEYUP:
-            if self.callbacks[evt].has_key(key):
-                if call in self.callbacks[evt][key]:
-                    self.callbacks[evt][key].remove(call)
-        else:
-            if call in self.callbacks[evt]:
-                self.callbacks[evt].remove(call)
-
     def change_game(self,game):
         self.game.exit()
         self.game = game
@@ -64,13 +44,13 @@ class App:
 
     def process_events(self, t):
         for event in pygame.event.get():
-            if self.callbacks.has_key(event.type):
+            if callbacks.has_key(event.type):
                 if (event.type == KEYDOWN or event.type == KEYUP):
-                    if self.callbacks[event.type].has_key(event.key):
-                        for call in self.callbacks[event.type][event.key]:
+                    if callbacks[event.type].has_key(event.key):
+                        for call in callbacks[event.type][event.key]:
                             call(t)
                 else:
-                    for call in self.callbacks[event.type]:
+                    for call in callbacks[event.type]:
                         call(t)
 
     def draw(self):
--- src/game/menu.py
+++ src/game/menu.py
@@ -2,6 +2,8 @@ from pygame.locals import *
 import pygame
 
 from game import BaseGame, TestGame
+from utils.callbacks import *
+
 
 from utils import Vector
 
@@ -16,13 +18,13 @@ class Menu(BaseGame):
         self.app.screen.blit(self.button, dest, self.app.screen.get_rect())
 
 
-        self.app.register_event(KEYDOWN, self.change_to_test_game , K_n)
+        register_event(KEYDOWN, self.change_to_test_game , K_n)
 
     def update(self, t):
         pass
 
     def exit(self):
-        self.app.unregister_event(KEYDOWN, self.change_to_test_game , K_n)
+        unregister_event(KEYDOWN, self.change_to_test_game , K_n)
 
     def change_to_test_game(self, t):
         game = TestGame()
